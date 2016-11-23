746fe9674ed677cf910739fd96474836687448d3
Juliana Cavalcanti <jucorrea.mail@gmail.com>
2016-11-23 21:50:34 -0200

    removing computation of distance and transposing from the loop

--- ex1_gradientDescent/gradientDescent.m
+++ ex1_gradientDescent/gradientDescent.m
@@ -1,9 +1,8 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %GRADIENTDESCENT Performs gradient descent to learn theta
-%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
-%   taking num_iters gradient steps with learning rate alpha
+%   updates theta by taking num_iters gradient steps with learning rate alpha
+%   also keeps a vector tracking the cost in each step 
 
-% Initialize some useful values
 m = length(y); % number of training examples
 n = size(X,2); % number of features
 J_history = zeros(num_iters, 1);
@@ -11,9 +10,10 @@ J_history = zeros(num_iters, 1);
 for iter = 1:num_iters
     
     h =  X * theta;
+    dist = (h - y)'
     
     for j = 1:n,
-      theta(j) = theta(j) - (alpha / m) * sum((h - y)' * X(:,j));
+      theta(j) = theta(j) - alpha * sum(dist * X(:,j)) / m ;
     end;
 
     % Save the cost J in every iteration    
